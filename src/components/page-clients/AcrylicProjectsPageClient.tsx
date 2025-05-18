aabc522d9ba0e73e7fe1925b394a9b1bf852a94b
Junjie Wu <junjiewu0930@gmail.com>
2025-05-18 02:25:49 +0000

    In the projects page, I want in desktop 4 columns and in mobile 2 columns instead of 3 and 1 columns right now

--- src/components/page-clients/AcrylicProjectsPageClient.tsx
+++ src/components/page-clients/AcrylicProjectsPageClient.tsx
@@ -67,7 +67,7 @@ export function AcrylicProjectsClientContent() {
       </div>
 
       {filteredProjects.length > 0 ? (
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-2 lg:grid-cols-4 gap-8">
           {filteredProjects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
